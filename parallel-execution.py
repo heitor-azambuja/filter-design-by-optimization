041fa2371788edd05a07a6c00143ca4f43ae5639
azambuja_heitor <heitortazamba@gmail.com>
2022-07-18 11:31:51 -0300

    argparse.

--- parallel-execution.py
+++ parallel-execution.py
@@ -1,16 +1,26 @@
 import multiprocessing as mp
 import os
+import argparse
 
-# Create an individial process for everry script execution
-# Warning: This makes all my cpu corers go 100% for a while.
-# Run at your own risk.
+# Create an individial process for every script execution
+
+# WARNING: Running this script with a high number of processes 
+# will cause your cpu to go 100% for a while. Run at your own risk.
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--processes', type=int, default=8, 
+					help='Number of processes to run. Defaults to 8.\
+						WARNING: Running this script with a high number of processes'
+)
+
+args = parser.parse_args()
 
 FILE_1 = 'pso-digital-filter.py -s -o '
 FILE_2 = 'TRIBES-digital-filter.py -s -o '
 EXECUTIONS = 4
 ORDERS = [2, 6, 8, 10, 15, 20]
 # PROC_NUM = 2 * EXECUTIONS * len(ORDERS)
-PROC_NUM = 8
+PROC_NUM = args.processes
 
 all_processes = []
 
@@ -22,7 +32,7 @@ for order in ORDERS:
 def execute(process):                                                             
 	os.system(f'python3 {process}')   
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
 	process_pool = mp.Pool(processes = PROC_NUM)
 	process_pool.map(execute, all_processes)
